e31bba171e1bb97e8790f7d2f6ababad936281be
munnam77 <munnam77@gmail.com>
2025-03-19 06:16:06 +0900

    Enhance build.sh script with clear instructions and additional Laravel commands

--- build.sh
+++ build.sh
@@ -2,11 +2,22 @@
 # exit on error
 set -o errexit
 
+# Install PHP dependencies
 composer install --optimize-autoloader --no-dev
+
+# Clear all caches
+php artisan clear-compiled
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
+
+# Create storage symbolic link
 php artisan storage:link
+
+# Migrate database (--force to run in production)
+php artisan migrate --force
+
+# Cache for optimization
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache 
